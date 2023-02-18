6ac12ad4b6b2da3a160527d132ff7f1b71ca9384
KeN <ken.t.csai@gmail.com>
2023-02-18 22:41:22 +0900

    create User entity.

--- src/main/kotlin/com/ken1427/bulletinboardapi/kt/domain/User.kt
+++ src/main/kotlin/com/ken1427/bulletinboardapi/kt/domain/User.kt
@@ -1,14 +1,30 @@
 package com.ken1427.bulletinboardapi.kt.domain
 
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "Users")
 data class User(
-    val id: Int? = null,
-    val username: String,
-    val mailAddress: String,
-    val status: Status,
-    val createdAt: Long? = null,
-    val updatedAt: Long? = null
+    @Id
+    @GeneratedValue
+    val id: Int = 0,
+    @Column(name = "username", length = 30, nullable = false)
+    val username: String = "username",
+    @Column(name = "mail_address", length = 30, nullable = false)
+    val mailAddress: String = "",
+    @Column(name = "status", length = 30, nullable = false)
+    val status: String = Status.ENABLE.name,
+    @Column(name = "createdAt", nullable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    @Column(name = "updatedAt", nullable = false)
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 ) {
-    enum class Status {
+    enum class Status{
         ENABLE,
         DISABLE
     }
--- src/main/kotlin/com/ken1427/bulletinboardapi/kt/usecase/user/UserResponse.kt
+++ src/main/kotlin/com/ken1427/bulletinboardapi/kt/usecase/user/UserResponse.kt
@@ -1,21 +1,22 @@
 package com.ken1427.bulletinboardapi.kt.usecase.user
 
 import com.ken1427.bulletinboardapi.kt.domain.User
+import java.time.LocalDateTime
 
 data class UserResponse(
-    val userId: Int?,
+    val userId: Int,
     val username: String,
     val mailAddress: String,
     val status: User.Status,
-    val createdAt: Long?,
-    val updatedAt: Long?
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
 ) {
     companion object {
         fun create(user: User) = UserResponse(
             userId = user.id,
             username = user.username,
             mailAddress = user.mailAddress,
-            status = user.status,
+            status = User.Status.valueOf(user.status),
             createdAt = user.createdAt,
             updatedAt = user.updatedAt
         )
