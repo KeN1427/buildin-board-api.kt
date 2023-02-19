05ac5572166ff7fccf89e61d281a956d113d1922
KeN <ken.t.csai@gmail.com>
2023-02-19 22:54:50 +0900

    update user controller response.

--- src/main/kotlin/com/ken1427/bulletinboardapi/kt/controller/UserController.kt
+++ src/main/kotlin/com/ken1427/bulletinboardapi/kt/controller/UserController.kt
@@ -1,9 +1,15 @@
 package com.ken1427.bulletinboardapi.kt.controller
 
 import com.ken1427.bulletinboardapi.kt.usecase.user.CreateUserUseCase
+import com.ken1427.bulletinboardapi.kt.usecase.user.DeleteUserUseCase
+import com.ken1427.bulletinboardapi.kt.usecase.user.GetUserUseCase
+import com.ken1427.bulletinboardapi.kt.usecase.user.UpdateUserUseCase
 import com.ken1427.bulletinboardapi.kt.usecase.user.UserRequest
+import com.ken1427.bulletinboardapi.kt.usecase.user.UserResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,21 +18,25 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.constraints.Null
 
 @RestController
 @RequestMapping("/v1")
 class UserController(
-    private val createUserUseCase: CreateUserUseCase
+    private val createUserUseCase: CreateUserUseCase,
+    private val getUserUseCase: GetUserUseCase,
+    private val updateUserUseCase: UpdateUserUseCase,
+    private val deleteUserUseCase: DeleteUserUseCase
 ) {
     @PostMapping(path = ["/users"], headers = ["Content-Type=application/json"])
     @Operation(summary = "Create a user.")
     fun createUser(
         @RequestBody(required = true)
-        @Parameter(description = "A user information", example = "{username: Bob, mail_address: xyz@example.com}")
+        @Parameter(description = "A user information", example = """{"username": "Bob", "mail_address": "xyz@example.com"}""")
         body: UserRequest
-    ): String {
-        createUserUseCase.handle(body)
-        return "${body.username}"
+    ): ResponseEntity<UserResponse> {
+        val result = createUserUseCase.handle(body)
+        return ResponseEntity(result, HttpStatus.OK)
     }
 
     @GetMapping("/users/{userId}")
@@ -35,8 +45,9 @@ class UserController(
         @Parameter(description = "user id", required = true)
         @PathVariable("userId")
         userId: String
-    ): String {
-        return "Get user information."
+    ): ResponseEntity<UserResponse> {
+        val result = getUserUseCase.handle(userId.toInt())
+        return ResponseEntity(result, HttpStatus.OK)
     }
 
     @PutMapping("/users/{userId}")
@@ -44,9 +55,13 @@ class UserController(
     fun updateUser(
         @Parameter(description = "user id", required = true)
         @PathVariable("userId")
-        userId: String
-    ): String {
-        return "Update user information."
+        userId: String,
+        @RequestBody(required = true)
+        @Parameter(description = "A user information", example = "{username: Bob, mail_address: xyz@example.com}")
+        body: UserRequest
+    ): ResponseEntity<UserResponse> {
+        val result = updateUserUseCase.handle(userId.toInt(), body)
+        return ResponseEntity(result, HttpStatus.OK)
     }
 
     @DeleteMapping("/users/{userId}")
@@ -55,7 +70,7 @@ class UserController(
         @Parameter(description = "user id", required = true)
         @PathVariable("userId")
         userId: String
-    ): String {
-        return "Delete a user."
+    ): ResponseEntity<Null> {
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 }
