731073bbae579bab0396f92987d050548376995f
KeN <ken.t.csai@gmail.com>
2023-02-20 21:57:22 +0900

    User repository modified to inherit Jpa repository.

--- src/main/kotlin/com/ken1427/bulletinboardapi/kt/repository/UserRepository.kt
+++ src/main/kotlin/com/ken1427/bulletinboardapi/kt/repository/UserRepository.kt
@@ -1,8 +1,8 @@
 package com.ken1427.bulletinboardapi.kt.repository
 
 import com.ken1427.bulletinboardapi.kt.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface UserRepository {
-    fun get(userId: Int): User
-    fun create(user: User): User
-}
+@Repository
+interface UserRepository: JpaRepository<User, Int> {}
