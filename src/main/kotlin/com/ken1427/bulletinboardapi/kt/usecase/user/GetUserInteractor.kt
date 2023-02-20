fc9c8127375dba3ae8fbe05c06810415c0ab768d
KeN <ken.t.csai@gmail.com>
2023-02-20 21:56:18 +0900

    create logic for getting user use case.

--- src/main/kotlin/com/ken1427/bulletinboardapi/kt/usecase/user/GetUserInteractor.kt
+++ src/main/kotlin/com/ken1427/bulletinboardapi/kt/usecase/user/GetUserInteractor.kt
@@ -1,5 +1,6 @@
 package com.ken1427.bulletinboardapi.kt.usecase.user
 
+import com.ken1427.bulletinboardapi.kt.domain.User
 import com.ken1427.bulletinboardapi.kt.repository.UserRepository
 import org.springframework.stereotype.Service
 
@@ -8,6 +9,7 @@ class GetUserInteractor(
     private val userRepository: UserRepository
 ): GetUserUseCase {
     override fun handle(userId: Int): UserResponse {
-        TODO("Not yet implemented")
+        val result = userRepository.findById(userId).get()
+        return UserResponse.create(result)
     }
 }
