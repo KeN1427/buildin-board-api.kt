93c2ea4a813e944d5ac2f742afcce3c5343c41e7
KeN <ken.t.csai@gmail.com>
2023-03-09 20:00:10 +0900

    feat: implement GetCommentUseCase.

---
+++ src/main/kotlin/com/ken1427/bulletinboardapi/kt/usecase/comment/GetCommentUseCase.kt
@@ -0,0 +1,24 @@
+package com.ken1427.bulletinboardapi.kt.usecase.comment
+
+import com.ken1427.bulletinboardapi.kt.repository.CommentRepository
+import com.ken1427.bulletinboardapi.kt.repository.ThreadRepository
+import org.springframework.stereotype.Service
+
+interface GetCommentUseCase {
+    fun handle(commentId: Int): CommentResponse
+}
+
+@Service
+class GetCommentUseCaseImpl(
+    private val commentRepository: CommentRepository
+): GetCommentUseCase {
+    override fun handle(commentId: Int): CommentResponse {
+        val comment = try {
+            commentRepository.getComment(commentId)
+        } catch (e: Exception) {
+            throw e
+        }
+
+        return CommentResponse.create(comment)
+    }
+}
